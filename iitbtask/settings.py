d1a7f24c3fc3628af7d8523185f01a69cb7be507
supradeep2819 <supradeep.c@valmi.io>
2024-10-09 12:16:40 +0530

    fix:render changes for settings.py

--- iitbtask/settings.py
+++ iitbtask/settings.py
@@ -140,6 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
